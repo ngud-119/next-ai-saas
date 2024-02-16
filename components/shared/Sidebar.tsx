c4ba6ec44b329e8b8d7958fda40cd430405ee83c
ngud-119 <passiondev7@gmail.com>
2024-02-16 23:18:19 +0200

    updated SideBar component; added button & SignedOut

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { navLinks } from "@/constants";
-import { SignedIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
 const SideBar = () => {
   const pathname = usePathname();
@@ -24,12 +25,30 @@ const SideBar = () => {
                 return (
                   <li key={link.route}
                     className={`sidebar-nav_element group ${isActive ? "bg-purple-gradient text-white" : "text-gray-700"}`}>
-                    <Link href={link.route}>{link.label}</Link>
+                    <Link className="sidebar-link" href={link.route}>
+                      <Image
+                        src={link.icon}
+                        alt="menu icon"
+                        width={24}
+                        height={24}
+                        className={`${isActive && "brightness-200"}`}
+                      />
+                      {link.label}
+                    </Link>
                   </li>
                 )
               })}
+              <li className="p-4 flex-center gap-2 cursor-pointer">
+                <UserButton afterSignOutUrl="/" showName />
+              </li>
             </ul>
           </SignedIn>
+
+          <SignedOut>
+            <Button asChild className="button bg-purple-gradient bg-cover">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </nav>
       </div>
     </aside >
