39bb98515ce51b02354970601df98c93b937c5ea
ngud-119 <passiondev7@gmail.com>
2024-02-16 22:20:04 +0200

    added appearance to ClerkProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      variables: {
+        colorPrimary: "#624cf5"
+      }
+    }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
