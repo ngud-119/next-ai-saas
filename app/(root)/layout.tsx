30c550130bc630187bf16caba62ce3de4039b9a4
ngud-119 <passiondev7@gmail.com>
2024-02-16 22:58:23 +0200

    added SideBar to Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,11 @@
+import SideBar from "@/components/shared/Sidebar";
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <SideBar />
+
+      {/* Mobile navigation */}
       <div className="root-container">
         <div className="wrapper">
           {children}
