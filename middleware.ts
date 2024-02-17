c0e4dd48623e922a7293188ff36d95d5af083fed
ngud-119 <passiondev7@gmail.com>
2024-02-17 14:49:09 +0200

    implemented webhooks

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ['/anyone-can-visit-this-route'],
+  publicRoutes: ['/api/webhooks/clerk'],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ['/no-auth-in-this-route'],
