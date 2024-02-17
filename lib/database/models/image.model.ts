b3912e4d232a48093e24e2ac520473dc3fe71650
ngud-119 <passiondev7@gmail.com>
2024-02-17 12:28:37 +0200

    Setup MongoDB: added MongooseConnection & connectToDatabase func; added models: Image, User, Transaction

---
+++ lib/database/models/image.model.ts
@@ -0,0 +1,43 @@
+import { Document, Schema, model, models } from "mongoose";
+
+export interface IImage extends Document {
+  title: string;
+  transformationType: string;
+  publicId: string;
+  secureURL: string;
+  width?: number;
+  height?: number;
+  config?: object;
+  transformationUrl?: string;
+  aspectRatio?: string;
+  color?: string;
+  prompt?: string;
+  author: {
+    _id: string;
+    firstName: string;
+    lastName: string;
+  }
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const ImageShema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureUrl: { type: URL, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: URL },
+  ascpectRatio: { type: String },
+  color: { type: String },
+  prompt: { type: String },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updateAt: { type: Date, default: Date.now },
+});
+
+const Image = models?.Image || model("Image", ImageShema);
+
+export default Image;
---
+++ lib/database/mongoose.ts
@@ -0,0 +1,33 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+interface MongooseConnection {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!MONGODB_URL) {
+    throw new Error("Missing MONGODB_URL environment variable inside .env.local");
+  }
+
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URL), {
+    dbName: "imagenko_db",
+    bufferCommands: false
+  };
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+} 
