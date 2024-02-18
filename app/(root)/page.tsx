a3a7ad25136ff2434c766a6da9aed5e8c2b28841
ngud-119 <passiondev7@gmail.com>
2024-02-18 14:03:18 +0200

    added Collection component render & searchParams

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,15 @@
+import { Collection } from "@/components/shared/Collection";
 import { navLinks } from "@/constants";
+import { getAllImages } from "@/lib/actions/image.actions";
 import Image from "next/image";
 import Link from "next/link";
 
-const Home = () => {
+const Home = async ({ searchParams }: SearchParamProps) => {
+  const page = Number(searchParams?.page) || 1;
+  const searchQuery = (searchParams?.query as string) || '';
+
+  const images = await getAllImages({ page, searchQuery });
+
   return (
     <>
       <section className="home">
@@ -32,6 +39,15 @@ const Home = () => {
             </li>
           ))}
         </ul>
+      </section>
+
+      <section className="sm:mt-12">
+        <Collection
+          hasSearch={true}
+          images={images?.data}
+          totalPages={images?.totalPage}
+          page={page}
+        />
       </section>
     </>
   );
