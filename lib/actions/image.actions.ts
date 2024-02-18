b6d8ce3fd45ba042bc7f0cd74e53ef066e41642a
ngud-119 <passiondev7@gmail.com>
2024-02-18 13:27:52 +0200

    implemented getAllImages function in image actions

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -6,6 +6,7 @@ import { handleError } from "../utils";
 import User from "../database/models/user.model";
 import Image from "../database/models/image.model";
 import { redirect } from "next/navigation";
+import { v2 as cloudinary } from "cloudinary";
 
 const populateUser = (query: any) => query.populate({
   path: "author",
@@ -89,6 +90,64 @@ export async function getImageById(imageId: string) {
     }
 
     return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error)
+  }
+}
+
+// Get all images
+export async function getAllImages({ limit = 9, page = 1, searchQuery = "" }: {
+  limit?: number,
+  page: number,
+  searchQuery?: string
+}) {
+  try {
+    await connectToDatabase();
+
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true
+    })
+
+    let expression = "folder=imagenko";
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`;
+    };
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {};
+
+    if (searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds
+        }
+      };
+    };
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find(query))
+      .sort({ upDated: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+      savedImages
+    };
   } catch (error) {
     handleError(error)
   }
