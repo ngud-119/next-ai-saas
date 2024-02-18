d8c9a321efcb9d5affa39c56d0317b51a59f2245
ngud-119 <passiondev7@gmail.com>
2024-02-18 17:14:02 +0200

    fixed: added clerkId to select in populateUser

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -11,7 +11,7 @@ import { v2 as cloudinary } from "cloudinary";
 const populateUser = (query: any) => query.populate({
   path: "author",
   model: User,
-  select: "_id firstName lastName"
+  select: "_id firstName lastName clerkId"
 });
 
 // Add image to database
