82835af757ed34970711b6b88f9b83d51695a83e
ngud-119 <passiondev7@gmail.com>
2024-02-18 15:01:53 +0200

    implemented downloadHandler function so that the user can download Transformed image

--- components/shared/TransformedImage.tsx
+++ components/shared/TransformedImage.tsx
@@ -1,14 +1,21 @@
 "use client";
 
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
-import { CldImage } from "next-cloudinary";
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
+import React from "react";
 
 const TransformedImage = ({ image, type, title, transformationConfig, isTransforming, setIsTransforming, hasDownload = false }: TransformedImageProps) => {
-  const downloadHandler = () => {
-
-  }
+  const downloadHandler = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    download(getCldImageUrl({
+      width: image?.width,
+      height: image?.height,
+      src: image?.publicId,
+      ...transformationConfig
+    }), title);
+  };
 
   return (
     <div className="flex flex-col gap-4">
